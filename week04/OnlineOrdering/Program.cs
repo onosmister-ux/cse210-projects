e6f76e6d14aa7191ef949ec5c580269fee090124
Rainmaker <onosmister@gmail.com>
2026-06-05 17:35:55 +0100

    finish week 4 assignment part 2

--- week04/OnlineOrdering/Program.cs
+++ week04/OnlineOrdering/Program.cs
@@ -1,9 +1,23 @@
-using System;
-
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello World! This is the OnlineOrdering Project.");
+        Address address1 = new Address("12 Lagos Street", "Lagos", "LA", "Nigeria");
+        Customer customer1 = new Customer("John Doe", address1);
+
+        Product p1 = new Product("Phone", "P100", 200, 1);
+        Product p2 = new Product("Headphones", "H200", 50, 2);
+
+        Order order1 = new Order(customer1);
+        order1.AddProduct(p1);
+        order1.AddProduct(p2);
+
+        Console.WriteLine("PACKING LABEL:");
+        Console.WriteLine(order1.GetPackingLabel());
+
+        Console.WriteLine("SHIPPING LABEL:");
+        Console.WriteLine(order1.GetShippingLabel());
+
+        Console.WriteLine("TOTAL COST: $" + order1.GetTotalCost());
     }
 }
